ad91316dfd4f08546cadd4f1378799fd38d06822
OscarISTUedu <f8keakk@gmail.com>
2025-04-14 22:28:30 +0800

    fix load hidden grid ui class

--- Grid/HiddenGridUI.Designer.cs
+++ Grid/HiddenGridUI.Designer.cs
@@ -1468,7 +1468,7 @@
             this.Controls.Add(this.groupBox5);
             this.Controls.Add(this.tabControl1);
             this.Name = "GridUI";
-            this.Load += new System.EventHandler(this.GridUI_Load);
+            //this.Load += new System.EventHandler(this.GridUI_Load);
             this.Resize += new System.EventHandler(this.GridUI_Resize);
             this.groupBox5.ResumeLayout(false);
             this.groupBox5.PerformLayout();
